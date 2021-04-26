433c608d12688f7e03fd0337434204d9262f8d15
Ao0Nv <54522480+Ao0Nv@users.noreply.github.com>
2021-04-26 14:37:32 +0900

    Add files via upload

--- reservation/reservation.php
+++ reservation/reservation.php
@@ -3,7 +3,6 @@
  <meta charset="UTF-8">
  <meta http-equiv="X-UA-Compatible" content="IE=edge">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <link name="viewport" content="width=device-width, initial-scale=1.0">
  <link rel="stylesheet" type="text/css" href="/include/header.css">
  <link rel="stylesheet" type="text/css" href="/include/footer.css"> 
  <title>会議室・備品予約システム</title>
