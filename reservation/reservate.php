983f54f428b22858cdd0925c31bf9b320cdfb740
Ao0Nv <54522480+Ao0Nv@users.noreply.github.com>
2021-05-17 05:32:36 +0900

    Add files via upload

--- reservation/reservate.php
+++ reservation/reservate.php
@@ -10,6 +10,7 @@
 </head>
 <?php include(dirname(__FILE__). '/include/header.php'); ?>
 <body>
+    <h2>予約情報確認ページ</h2>
     <?
 
 
@@ -21,37 +22,41 @@
         $purpose = "";
         $code = 0;
 
+        $conf_room = "";
+        $equipment = "";
+        $equipment_num = "";
+
         if(isset($_POST["ok"]))
         {
-            $registant = $_SESSION['registant'];
-            $date = $_SESSION['date'];
-            $start = $_SESSION['start'];
-            $finish = $_SESSION['finish'];
-            $num_of_people = $_SESSION['num_of_people'];
-            $purpose = $_SESSION['purpose'];
-            $status = "reserved";
+            $registant_inp = $_SESSION['registant'];
+            $date_inp = $_SESSION['date'];
+            $start_inp = $_SESSION['start'];
+            $finish_inp = $_SESSION['finish'];
+            $num_of_people_inp = $_SESSION['num_of_people'];
+            $purpose_inp = $_SESSION['purpose'];
+            $status_inp = "reserved";
             $code = 0;
 
-            $conf_room = $_SESSION['conf_room'];
-            $equipment = $_SESSION['equipment'];
-            $equipment_num = $_SESSION['equipment_num'];
+            $conf_room_inp = $_SESSION['conf_room'];
+            $equipment_inp = $_SESSION['equipment'];
+            $equipment_num_inp = $_SESSION['equipment_num'];
 
             
             $connect = connect_db();
-            $stmt_resv = $connect->prepare("INSERT INTO reservation VALUES(:code, :date, :start, :finish, :redistrant, :num_of_people)");
-            $stmt_conf = $connect->prepare("INSERT INTO conference VALUES(:conf_room)");
-            $stmt_eqpm = $connect->prepare("INSERT INTO equipment VALUES(:equipment, :equipment_num)");
+            $stmt_reservation = $connect->prepare("INSERT INTO reservation VALUES(:code, :date_inp, :start_inp, :finish_inp, :redistrant_inp, :num_of_people_inp)");
+            $stmt_conference_room = $connect->prepare("INSERT INTO conference VALUES(:conf_room_inp)");
+            $stmt_equipment = $connect->prepare("INSERT INTO equipment VALUES(:equipment_inp, :equipment_num_inp)");
 
-            $stmt_reservation -> bindParam(":redistrant", $redistrant, PDO::PARAM_STR);
-            $stmt_reservation -> bindParam(":date", $date, PDO::PARAM_STR);
-            $stmt_reservation -> bindParam(":start", $start, PDO::PARAM_INT);
-            $stmt_reservation -> bindParam(":finish", $finish, PDO::PARAM_INT);
-            $stmt_reservation -> bindParam(":num_of_people", $num_of_people, PDO::PARAM_INT);
-            $stmt_reservation -> bindParam(":purpose", $purpose, PDO::PARAM_STR);
+            $stmt_reservation -> bindParam(":redistrant", $redistrant_inp, PDO::PARAM_STR);
+            $stmt_reservation -> bindParam(":date", $date_inp, PDO::PARAM_STR);
+            $stmt_reservation -> bindParam(":start", $start_inp, PDO::PARAM_INT);
+            $stmt_reservation -> bindParam(":finish", $finish_inp, PDO::PARAM_INT);
+            $stmt_reservation -> bindParam(":num_of_people", $num_of_people_inp, PDO::PARAM_INT);
+            $stmt_reservation -> bindParam(":purpose", $purpose_inp, PDO::PARAM_STR);
 
-            $stmt_conference_room -> bindParam(":conf_room", $conf_room, PDO::PARAM_STR);
-            $stmt_equipment -> bindParam(":equipment", $equipment, PDO::PARAM_STR);
-            $stmt_equipment -> bindParam(":equipment", $equipment_num, PDO::PARAM_INT);
+            $stmt_conference_room -> bindParam(":conf_room", $conf_room_inp, PDO::PARAM_STR);
+            $stmt_equipment -> bindParam(":equipment", $equipment_inp, PDO::PARAM_STR);
+            $stmt_equipment -> bindParam(":equipment", $equipment_num_inp, PDO::PARAM_INT);
             
             $stmt_reservation -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $stmt_conference_room ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -75,26 +80,26 @@
         else
         {
 
-            if(input_check($registant, 'registrant') and input_check_b($date, 'date') and
-                    input_check_b($start, 'start') and input_check_b($finish, 'finish') and 
-                    input_check_b($num_of_people, 'num_of_people') and input_check_b($purpose, 'purpose'))
+            if(input_check($registant_inp, 'registrant') and input_check_b($date_inp, 'date') and
+                    input_check_b($start_inp, 'start') and input_check_b($finish_inp, 'finish') and 
+                    input_check_b($num_of_people_inp, 'num_of_people') and input_check_b($purpose_inp, 'purpose'))
                 {
-                    $_SESSION['registant'] = $registant;
-                    $_SESSION['date'] = $date;
-                    $_SESSION['start'] = $start;
-                    $_SESSION['finish'] = $finish;
-                    $_SESSION['num_of_people'] = $num_of_people;
-                    $_SESSION['purpose'] = $purpose;
+                    $_SESSION['registant'] = $registant_inp;
+                    $_SESSION['date'] = $date_inp;
+                    $_SESSION['start'] = $start_inp;
+                    $_SESSION['finish'] = $finish_inp;
+                    $_SESSION['num_of_people'] = $num_of_people_inp;
+                    $_SESSION['purpose'] = $purpose_inp;
 
                     print "<h1></h1>";
-                    print "<p>利用者名:". $registant. "</p>";
-                    print "<p>会議室:". $date. "</p>";
-                    print "<p>備品:". $start. "</p>";
-                    print "<p>備品数:". $finish. "</p>";
-                    print "<p>予約日:". $num_of_peolr. "</p>";
-                    print "<p>予約時間:". $first. "~" . $finish ."</p>";
-                    print "<p>人数:". $num_of_people. "</p>";
-                    print "<p>目的:". $purpose. "</p>";
+                    print "<p>利用者名:". $registant_inp. "</p>";
+                    print "<p>会議室:". $date_inp. "</p>";
+                    print "<p>備品:". $start_inp. "</p>";
+                    print "<p>備品数:". $finish_inp. "</p>";
+                    print "<p>予約日:". $num_of_people_inp. "</p>";
+                    print "<p>予約時間:". $first_inp. "~" . $finish_inp."</p>";
+                    print "<p>人数:". $num_of_people_inp. "</p>";
+                    print "<p>目的:". $purpose_inp. "</p>";
                     
                     print "上記の情報で予約しますか？";
                     print "<form  method=\"post\">\n";
