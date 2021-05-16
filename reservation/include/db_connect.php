270248f44f809bc31cdc75bebe4c187fb1b81184
Ao0Nv <54522480+Ao0Nv@users.noreply.github.com>
2021-05-17 05:13:17 +0900

    Add files via upload

--- reservation/include/db_connect.php
+++ reservation/include/db_connect.php
@@ -1,10 +1,10 @@
 <?php
     function connect_db(){
-        $server = "ec2-3-89-230-115.compute-1.amazonaws.com";
-        $database = "d23l6otnkrusl3";
+        $server = "ec2-54-90-13-87.compute-1.amazonaws.com";
+        $database = "d1gsgcv5rqsvoc";
         $port_number = 5432;
-        $user_id = "rdnkafrobyeogc";
-        $user_password = "8360e5af63c0c83be49cdf918f76034aec49c1370c63c12d8e58f192a0310cb8";
+        $user_id = "embzsaahgcxswh";
+        $user_password = "007dc962782174807e8f71439970a83caefffb2de91754969788625aff742c1d";
         
         $connect = new PDO("pgsql:host=$server;dbname=$database;port=$port_number;user=$user_id;password=$user_password");
 
