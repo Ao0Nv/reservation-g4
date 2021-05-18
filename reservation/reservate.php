6331c943dd6864d003aa13edff04b90c4b0ffb3f
Ao0Nv <54522480+Ao0Nv@users.noreply.github.com>
2021-05-18 12:51:54 +0900

    Add files via upload

--- reservation/reservate.php
+++ reservation/reservate.php
@@ -39,25 +39,29 @@
         $num_of_people_inp = $_POST["num_of_people"];
         $purpose_inp = $_POST["purpose"];
         
-        $connect = connect_db();
-        $stmt_reservation = $connect->prepare("INSERT INTO reservation VALUES(:code, :rsv_date, :rsv_start, :rsv_finish, :rsv_redistrant, :rsv_num_of_people)");
-        $stmt_conference_room = $connect->prepare("INSERT INTO conference VALUES(:rsv_conf_room)");
-        $stmt_equipment = $connect->prepare("INSERT INTO equipment VALUES(:rsv_equipment, :rsv_equipment_num)");
+        /*
+        if(isset($_POST["ok"]))
+        {
+            
+            $connect = connect_db();
+            $stmt_reservation = $connect->prepare("INSERT INTO reservation VALUES(:code, :rsv_date, :rsv_start, :rsv_finish, :rsv_redistrant, :rsv_num_of_people)");
+            $stmt_conference_room = $connect->prepare("INSERT INTO conference VALUES(:rsv_conf_room)");
+            $stmt_equipment = $connect->prepare("INSERT INTO equipment VALUES(:rsv_equipment, :rsv_equipment_num)");
 
-        $stmt_reservation -> bindParam(":rsv_redistrant", $redistrant_inp, PDO::PARAM_STR);
-        $stmt_reservation -> bindParam(":rsv_date", $date_inp, PDO::PARAM_STR);
-        $stmt_reservation -> bindParam(":rsv_start", $start_inp, PDO::PARAM_STR);
-        $stmt_reservation -> bindParam(":rsv_finish", $finish_inp, PDO::PARAM_STR);
-        $stmt_reservation -> bindParam(":rsv_num_of_people", $num_of_people_inp, PDO::PARAM_INT);
-        $stmt_reservation -> bindParam(":rsv_purpose", $purpose_inp, PDO::PARAM_STR);
+            $stmt_reservation -> bindParam(":rsv_redistrant", $redistrant_inp, PDO::PARAM_STR);
+            $stmt_reservation -> bindParam(":rsv_date", $date_inp, PDO::PARAM_STR);
+            $stmt_reservation -> bindParam(":rsv_start", $start_inp, PDO::PARAM_STR);
+            $stmt_reservation -> bindParam(":rsv_finish", $finish_inp, PDO::PARAM_STR);
+            $stmt_reservation -> bindParam(":rsv_num_of_people", $num_of_people_inp, PDO::PARAM_INT);
+            $stmt_reservation -> bindParam(":rsv_purpose", $purpose_inp, PDO::PARAM_STR);
 
-        $stmt_conference_room -> bindParam(":rsv_conf_room", $conf_room_inp, PDO::PARAM_STR);
-        $stmt_equipment -> bindParam(":rsv_equipment", $equipment_inp, PDO::PARAM_STR);
-        $stmt_equipment -> bindParam(":rsv_equipment_num", $equipment_num_inp, PDO::PARAM_INT);
-        
-        $stmt_reservation -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $stmt_conference_room ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $stmt_equipment -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $stmt_conference_room -> bindParam(":rsv_conf_room", $conf_room_inp, PDO::PARAM_STR);
+            $stmt_equipment -> bindParam(":rsv_equipment", $equipment_inp, PDO::PARAM_STR);
+            $stmt_equipment -> bindParam(":rsv_equipment_num", $equipment_num_inp, PDO::PARAM_INT);
+            
+            $stmt_reservation -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $stmt_conference_room ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $stmt_equipment -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             
             try
             {
@@ -74,18 +78,20 @@
             {
                 exit($e->getMessage());
             }
-        
-        
-            if(1)/*input_check($registant_inp, 'registrant') and input_check($date_inp, 'date') and
+        }
+        else
+        {
+
+            if(1)input_check($registant_inp, 'registrant') and input_check($date_inp, 'date') and
                     input_check($start_inp, 'start') and input_check($finish_inp, 'finish') and 
-                    input_check($num_of_people_inp, 'num_of_people') and input_check($purpose_inp, 'purpose'))
-                {
-                    $_SESSION['registant'] = $registant_inp;
+                    input_check($num_of_people_inp, 'num_of_people') and input_check($purpose_inp, 'purpose'))*/
+                //{
+                    /*$_SESSION['registant'] = $registant_inp;
                     $_SESSION['date'] = $date_inp;
                     $_SESSION['start'] = $start_inp;
                     $_SESSION['finish'] = $finish_inp;
                     $_SESSION['num_of_people'] = $num_of_people_inp;
-                    $_SESSION['purpose'] = $purpose_inp;
+                    $_SESSION['purpose'] = $purpose_inp;*/
 
                     print "<br>";
                     print "<h1></h1>";
@@ -105,13 +111,13 @@
                     print "<input type=\"submit\" formaction=\"index.php\" name=\"ok\" value=\"はい\">\n";
                     print "<input type=\"submit\" formaction=\"reservation.php\"value=\"いいえ\">\n";
                     print "</form>\n";
-                }
+                /*}
                 else
                 {
                     //header("Location: reservation.php");
                     exit();
                 }
-
+        }*/
     ?>
 </body>
 <?php include(dirname(__FILE__). '/include/footer.php'); ?>
