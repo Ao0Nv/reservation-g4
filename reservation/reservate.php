5b1660010120e7c4cb0c97160969199a2a33c382
Ao0Nv <54522480+Ao0Nv@users.noreply.github.com>
2021-05-18 11:36:11 +0900

    Add files via upload

--- reservation/reservate.php
+++ reservation/reservate.php
@@ -49,10 +49,11 @@
         $count=$stmt->rowCount();
         echo $count;
         */
+        $code=1;
         
         try
         {
-            $sql="INSERT INTO reservation VALUES('$code', '$date_inp', '$start_inp', '$finish_inp', '$registant_inp', '$num_of_people_inp', '$purpose_inp', 'wait')";
+            $sql="INSERT INTO reservation VALUES($code, $date_inp, $start_inp, $finish_inp, $registant_inp, $num_of_people_inp, $purpose_inp, 'wait')";
             
             $res = $connect->query($sql);
             header("Location: reservation.php");
