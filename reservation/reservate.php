199919a24d94ecc84da09d1a26403d3fcbb125f8
Ao0Nv <54522480+Ao0Nv@users.noreply.github.com>
2021-05-18 14:11:37 +0900

    Add files via upload

--- reservation/reservate.php
+++ reservation/reservate.php
@@ -29,70 +29,100 @@
         $equipment_inp = "";
         $equipment_num_inp = "";
 
-        $registant_inp = $_POST["registant"];
-        $conf_room_inp = $_POST["conf_room"];
-        $equipment_inp = $_POST["equipment"];
-        $equipment_num_inp = $_POST["equipment_num"];
-        $date_inp = $_POST["date"];
-        $start_inp = $_POST["start"];
-        $finish_inp = $_POST["finish"];
-        $num_of_people_inp = $_POST["num_of_people"];
-        $purpose_inp = $_POST["purpose"];
         
-        $code = 1;
 
-        print "<br>";
-        print "<h1></h1>";
-        print "<p>利用者名:". $registant_inp. "</p>";
-        print "<p>会議室　:". $conf_room_inp. "</p>";
-        print "<p>備品　　:". $equipment_inp. "</p>";
-        print "<p>備品数　:". $equipment_num_inp. "</p>";
-        print "<p>予約日　:". $date_inp. "</p>";
-        print "<p>予約時間:". $start_inp. "~" . $finish_inp."</p>";
-        print "<p>人数　　:". $num_of_people_inp. "</p>";
-        print "<p>目的　　:". $purpose_inp. "</p>";
         
-        print "<br>";
 
-        print "上記の情報で予約しますか？";
-        print "<form  method=\"post\">\n";
-        print "<input type=\"submit\" formaction=\"index.php\" name=\"ok\" value=\"はい\">\n";
-        print "<input type=\"submit\" formaction=\"reservation.php\"value=\"いいえ\">\n";
-        print "</form>\n";
-
-        try
+        if(isset($_POST["ok"]))
         {
-            $connect = connect_db();
+            $registant_inp = $_POST['registant'];
+            $conf_room_inp = $_POST['conf_room'];
+            $equipment_inp = $_POST['equipment'];
+            $equipment_num_inp = $_POST['equipment_num'];
+            $date_inp = $_POST['date'];
+            $start_inp = $_POST['start'];
+            $finish_inp = $_POST['finish'];
+            $num_of_people_inp = $_POST['num_of_people'];
+            $purpose_inp = $_POST['purpose'];
         
-            $connect->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-            $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            
-            $sql = 'INSERT INTO reservation from VALUES(:code, :date_inp, :start_inp, :finish_inp, :redistrant_inp, :num_of_people_inp)';
+            $code = 1;
 
+            $connect = connect_db();
+
+            $sql = 'INSERT INTO reservation VALUES(:code, :date_inp, :start_inp, :finish_inp, :redistrant_inp, :num_of_people_inp)';
+            
             $stmt = $connect->prepare($sql);
+            
+            $stmt -> bindParam(":redistrant_inp", $redistrant_inp);
+            $stmt -> bindParam(":date_inp", $date_inp);
+            $stmt -> bindParam(":start_inp", $start_inp);
+            $stmt -> bindParam(":finish_inp", $finish_inp);
+            $stmt -> bindParam(":num_of_people_inp", $num_of_people_inp);
+            $stmt -> bindParam(":purpose_inp", $purpose_inp);
+            
+            //$stmt -> execute();
+            //$connect->commit();
+
+            $stmt->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+            
 
             try
             {
-                $stmt -> bindParam(":redistrant_inp", $redistrant_inp);
-                $stmt -> bindParam(":date_inp", $date_inp);
-                $stmt -> bindParam(":start_inp", $start_inp);
-                $stmt -> bindParam(":finish_inp", $finish_inp);
-                $stmt -> bindParam(":num_of_people_inp", $num_of_people_inp);
-                $stmt -> bindParam(":purpose_inp", $purpose_inp);
-
+                $count_query = $connect -> query("SELECT * FROM reservation");
+                $count = $count_query -> rowCount();
+                $code = $count * 1;
+                
                 $stmt -> execute();
 
-                $connect->commit();
+                header("Location: reservation.php");
+                exit();
+                
             }
             catch (PDOException $e)
             {
-                $dbh->rollback();
-		        throw $e;
+                exit($e->getMessage());
             }
+
         }
-        catch (PDOException $e)
+        else
         {
-            exit($e->getMessage());
+            if(input_check($registant_inp, 'registrant') and input_check($date_inp, 'date') and
+                    input_check($start_inp, 'start') and input_check($finish_inp, 'finish') and 
+                    input_check($num_of_people_inp, 'num_of_people') and input_check($purpose_inp, 'purpose'))
+            {
+                $_SESSION['registant'] = $registant_inp;
+                $_SESSION['date'] = $date_inp;
+                $_SESSION['start'] = $start_inp;
+                $_SESSION['finish'] = $finish_inp;
+                $_SESSION['num_of_people'] = $num_of_people_inp;
+                $_SESSION['purpose'] = $purpose_inp;
+            
+
+                print "<br>";
+                print "<h1></h1>";
+                print "<p>利用者名:". $registant_inp. "</p>";
+                print "<p>会議室　:". $conf_room_inp. "</p>";
+                print "<p>備品　　:". $equipment_inp. "</p>";
+                print "<p>備品数　:". $equipment_num_inp. "</p>";
+                print "<p>予約日　:". $date_inp. "</p>";
+                print "<p>予約時間:". $start_inp. "~" . $finish_inp."</p>";
+                print "<p>人数　　:". $num_of_people_inp. "</p>";
+                print "<p>目的　　:". $purpose_inp. "</p>";
+
+                print "<br>";
+
+                print "上記の情報で予約しますか？";
+                print "<form  method=\"post\">\n";
+                print "<input type=\"submit\" formaction=\"index.php\" name=\"ok\" value=\"はい\">\n";
+                print "<input type=\"submit\" formaction=\"reservation.php\"value=\"いいえ\">\n";
+                print "</form>\n";
+            }
+            else
+            {
+                header("Location: reservation.php");
+                exit();
+            }
         }
         
         
@@ -142,7 +172,7 @@
         else
         {
 
-            if(1)input_check($registant_inp, 'registrant') and input_check($date_inp, 'date') and
+            /*if(1)input_check($registant_inp, 'registrant') and input_check($date_inp, 'date') and
                     input_check($start_inp, 'start') and input_check($finish_inp, 'finish') and 
                     input_check($num_of_people_inp, 'num_of_people') and input_check($purpose_inp, 'purpose'))*/
                 //{
