1da437e5537bab6cf9987fe471f92d73a2b00117
Ao0Nv <54522480+Ao0Nv@users.noreply.github.com>
2021-05-22 04:20:03 +0900

    Add files via upload

--- reservation/input_confirm.php
+++ reservation/input_confirm.php
@@ -81,6 +81,7 @@
         <lavel for="purpose">目的 　 </lavel>
         <input type="text" name="purpose" size="100">
         <br>
+        <!--
         <table>
           <thead>
             <tr>
@@ -106,10 +107,53 @@
           </tbody>
           <tfoot>
             <tr>
-              <td ><button id="add" type="button"> + </button></td>
+              <td ><button id="add" type="button">追加</button></td>
             </tr>
           </tfoot>
         </table>
+        -->
+
+
+
+        <input type="hidden" name="count" id="count_id" value="0">
+        <table width="100%" cellpadding="3" cellspacing="1" id="table">
+            <tbody>
+                <tr>
+                    <th>備品</th>
+                    <th>備品数</th>
+                </tr>
+                <tr>
+                    <td><input type="button" value="入力欄追加" onclick="insertTable ()"></td>
+                </tr>
+                <tr>
+                    <td><input type='button' value='削除' onclick='deleteTable (getSort(this.parentNode.parentNode));'></td>
+                    <td>
+                        <select name='equipment'>
+                            <option value="null">選択してください</option>
+                            <option value='EPZON EP1010'>EPZON EP1010</option>
+                            <option value='EPZON EPS500'>EPZON EPS500</option>
+                            <option value='H実業　SKREEN60'>H実業　SKREEN60</option>
+                            <option value='H実業　SKREEN150'>H実業　SKREEN150</option>
+                            <option value='無線マイクロフォン'>無線マイクロフォン</option>
+                            <option value='OMKYO'>OMKYO</option>
+                            <option value='脚立'>脚立</option>
+                            <option value='巻コード'>巻コード</option>
+                        </select>
+                    </td>
+                    <td><input type='text' value="" name='equipment_num'></td>
+                    <td><input type='text' value="" name='params3_'></td>
+                    <td><input type='text' value="" name='params4_'></td>
+                </tr>
+            </tbody>
+    </table>
+<input type="button" value="実行" onclick="setParamsName (); submit ();" />
+
+
+
+
+
+
+
         <br>
         <input type="submit" value="予約確定">
     </form>
@@ -136,6 +180,7 @@
           document.getElementById("user").disabled = false;
         }
       }
+      /*
       $(function() 
       {
         $('button#add').click(function()
@@ -148,6 +193,122 @@
           $(tr_form).appendTo($('table > tbody'));
         });
       });
+      */
+
+
+      /**
+* 行追加
+*/
+function insertTable () 
+{
+    var table1 = document.getElementById ("table");
+    var row = table1.insertRow (2); // 行を追加（見出し、追加ボタンの次の行）
+    var cell0 = row.insertCell (0); // セルを追加
+    var cell1 = row.insertCell (1); // セルを追加
+    var cell2 = row.insertCell (2); // セルを追加
+    var cell3 = row.insertCell (3); // セルを追加
+    var cell4 = row.insertCell (4); // セルを追加
+    var cell5 = row.insertCell (5); // セルを追加
+    var cell6 = row.insertCell (6); // セルを追加
+    var cell7 = row.insertCell (7); // セルを追加
+    
+    //cell0.setAttribute ("style", "border: 1px solid #cccccc;"); //適当にスタイルを設定
+    //cell1.setAttribute ("style", "border: 1px solid #cccccc;"); //適当にスタイルを設定
+    //cell2.setAttribute ("style", "border: 1px solid #cccccc;"); //適当にスタイルを設定
+    //cell3.setAttribute ("style", "border: 1px solid #cccccc;"); //適当にスタイルを設定
+    //cell4.setAttribute ("style", "border: 1px solid #cccccc;"); //適当にスタイルを設定
+    //セルに内容を設定
+    cell0.innerHTML = "<input type='button' value='削除' onclick='deleteTable (getSort(this.parentNode.parentNode));'>";
+    cell1.innerHTML = "<select name='equipment'>"
+                    + "<option value='' > 選択してください </option>"
+                    + "<option value='EPZON EP1010'>EPZON EP1010</option>"
+                    + "<option value='EPZON EPS500'>EPZON EPS500</option>"
+                    + "<option value='H実業　SKREEN60'>H実業　SKREEN60</option>"
+                    + "<option value='H実業　SKREEN150'>H実業　SKREEN150</option>"
+                    + "<option value='無線マイクロフォン'>無線マイクロフォン</option>"
+                    + "<option value='OMKYO'>OMKYO</option>"
+                    + "<option value='脚立'>脚立</option>"
+                    + "<option value='巻コード'>巻コード</option>"
+                    + "</select>";
+    cell2.innerHTML = "<input type='text' value='' name = 'equipment_num' > ";
+    cell3.innerHTML = "<input type='text' value='' name = 'params3_' > ";
+    cell4.innerHTML = "<input type='text' value='' name = 'params4_' > ";
+}
+/**
+* 入力欄削除
+*/
+function deleteTable (row) 
+{
+    document.getElementById ("table").deleteRow (row);
+}
+/**
+* 順番を調べる
+*/
+function getSort (target) 
+{
+    var nodeLists = document.getElementById ("table").childNodes[1].childNodes;
+    var trCount = - 1;
+    for (var i = 0; i < nodeLists.length; i++) {
+        var node = nodeLists.item (i);
+        if (node.tagName == "TR") 
+        {
+            trCount++;
+        }
+        if (node == target) 
+        {
+            return trCount;
+        }
+    }
+    return 0;
+}
+/**
+* パラメータ名を設定
+*/
+function setParamsName () 
+{
+    // 項目数を設定
+    var nodeLists = document.getElementById ("table").childNodes[1].childNodes;
+    // 名前を設定
+    var cnt = 0 - 2; //見出し、追加ボタン分減算
+    for (var r = 0; r < nodeLists.length; r++) 
+    {
+        if (nodeLists.item (r).nodeName != "TR") 
+        {
+            continue;
+        }
+        var tdLists = nodeLists.item (r).childNodes;
+        for (var d = 0; d < tdLists.length; d++) 
+        {
+            if (tdLists.item (d).nodeName != "TD") 
+            {
+                continue;
+            }
+            // 内容を修正
+            var params = tdLists.item (d).childNodes;
+            for (var p = 0; p < params.length; p++) 
+            {
+                var node = params.item (p);
+                if (node.nodeName != "INPUT"
+                &&  node.nodeName != "TEXTAREA"
+                &&  node.nodeName != "SELECT") 
+                {
+                    continue;
+                }
+                if (node.name.lastIndexOf ("_") == node.name.length - 1) 
+                {
+                    node.name = node.name + cnt;
+                }
+            }
+        }
+        cnt++;
+    }
+    document.getElementById ("count_id").value = cnt;
+}
+
+
+
+
+
     </script>
   </main>
   <?php include(dirname(__FILE__). '/include/footer.php'); ?>
