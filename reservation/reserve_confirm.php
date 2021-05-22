1837020c0974de8da830cc13e78e2346ce32ae3c
Ao0Nv <54522480+Ao0Nv@users.noreply.github.com>
2021-05-22 10:47:33 +0900

    Add files via upload

--- reservation/reserve_confirm.php
+++ reservation/reserve_confirm.php
@@ -115,7 +115,7 @@
         }
         else
         {
-            if(1input_check($registant_inp, 'registrant') and input_check($date_inp, 'date') and
+            if(input_check($registant_inp, 'registrant') and input_check($date_inp, 'date') and
                 input_check($start_inp, 'start') and input_check($finish_inp, 'finish') and 
                 input_check($num_of_people_inp, 'num_of_people') and input_check($purpose_inp, 'purpose'))
             {
